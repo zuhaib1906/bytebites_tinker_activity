6eadeae179ea86b343b68f7ac7d4b6030949b012
Zuhaib Variyava <zuhaibv@mac.mynetworksettings.com>
2026-06-14 16:07:45 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,4 @@
+# Customer stores user information and purchase history.
+# FoodItem stores item details such as name, price, category, and popularity.
+# Menu manages a collection of FoodItem objects and supports category filtering.
+# Order groups selected FoodItem objects and calculates the total cost.
